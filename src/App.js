1906da70ddb9ec49a176ce513fdf76a9c0b36408
Saugat Rimal <saugatrimal60@gmail.com>
2022-06-27 21:51:54 +0545

    [ading

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Card from "./card";
 
 function App() {
   return (
-    <div className="flex h-auto para py-56">
+    <div className="flex h-auto para py-32 lg:py-56">
       <Card />
     </div>
   );
